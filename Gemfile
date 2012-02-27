099cc5e5439929f1b5ce03f0cfd0e44a56fd250a
Maggie Kean <maggie@kean.net>
2012-02-26 23:28:56 -0800

    up to chapter 5

--- Gemfile
+++ Gemfile
@@ -14,6 +14,10 @@ end
 group :test do
 	gem 'rspec'
 	gem 'webrat'
+	gem 'autotest'
+	gem 'autotest-rails-pure'
+	gem 'autotest-fsevent'
+	gem 'autotest-growl'
 end
 
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
 
   def home
-    @title = "Home"
+    @page_title = "Home"
   end
 
   def contact
-    @title = "Contact"
+    @page_title = "Contact"
   end
 
   def about
-    @title = "About"
+    @page_title = "About"
+  end
+  
+  def help
+    @page_title = "Help"
   end
 end
