70f2137de491e1e8e646805581418a2b00e669b0
Maggie Kean <maggie@kean.net>
2012-02-28 14:15:52 -0800

    fixed error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  validates :name, presence: true, length: { maximum: 50 }
-  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:   true,
-                    format:     { with: valid_email_regex },
-                    uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6}
+  validates_presence_of :name, :message => "Name is a required field"
+  validates_length_of :name, :maximum => 50, :message => "Maximum length for Name is 50 characters"
+  validates_presence_of :email, :message => "Email is a required field"
+  validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, :message => "Invalid email format"
+  validates_uniqueness_of :email, :case_sensitive => false, :message => "Someone with that email address is already registered"
+  validates_length_of :password, :minimum => 6, :message => "Password must be at least 6 characters long"
 end
